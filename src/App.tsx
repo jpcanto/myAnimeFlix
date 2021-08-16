7c3a8af27df1c25fd08761bcf6b6a4ecc9f2a9d1
jpcanto <canto.jplopes@gmail.com>
2021-08-15 23:07:32 -0300

    feat: add globalstyle and custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { theme } from 'styles/theme';
+import { GlobalStyle } from 'styles/global';
 
-import { Routes } from './routes';
+import { Routes } from 'routes';
+import { ThemeProvider } from '@material-ui/core';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
