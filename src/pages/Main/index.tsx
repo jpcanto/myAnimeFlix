02c40b2d2fd051421ebafa95ce8d0f19f995b1ab
jpcanto <canto.jplopes@gmail.com>
2021-08-15 23:07:58 -0300

    feat: add routes

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
+import { Navbar } from '../../components/shared/Navbar';
+
 export const Main: React.FC = () => {
-  return (
-    <h1>Main</h1>
-  );
-}
+  return <Navbar />;
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,4 +10,4 @@ export const Routes: React.FC = () => {
       <Route component={Profile} path="/profile" exact />
     </Switch>
   );
-}
+};
